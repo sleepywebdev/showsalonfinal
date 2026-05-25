481e6d61a3e2af546afdc04a2eb212251ea75c9e
sleepywebdev <157572916+sleepywebdev@users.noreply.github.com>
2026-05-25 18:42:36 +1000

    Fix index.tsx - proper React entry point

--- index.tsx
+++ index.tsx
@@ -1,10 +1,4 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./styles.css";
+import React from "react";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+export default <App />;
